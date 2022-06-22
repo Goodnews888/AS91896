2b7fa94dfd5b26f8dfab21ebba8de1922faafa94
Goodnews888 <101529980+Goodnews888@users.noreply.github.com>
2022-06-23 00:40:24 +1200

    FINAL V2 (Commenting & minor changes)

--- AS91896_main.py
+++ AS91896_main.py
@@ -1,12 +1,14 @@
 #Connor Ghezzi
-#28/05/22, Initalizing Repository
-#7/06/22, 
+
 
 #Importing tkinter to allow for GUI
-
 from tkinter import *
-from tkinter import messagebox
 from tkinter import ttk
+
+#Importing messagebox to allow for error messages if there is wrong input by the user
+from tkinter import messagebox
+
+#Importing random to allow for random generation of receipt number.
 import random
 
 #Quits the program upon clicking "Quit" Button.
@@ -32,29 +34,39 @@ def setup_buttons():
     #I have made a seperate button that displays the sub_window without having to enter any new details for a customer.
     Button(main_window, text ="Show Details", command = show_details, width =10) .grid(column =4, row =6, pady = 5, padx = 5)
 
-
+    #Added some empty labels for the purpose of the title being able rowspan to make it bigger, so they don't
+    # any of my other labels.
     Label(main_window, text ="").grid(column = 0, row =1)
     Label(main_window, text ="").grid(column = 0, row =2)
 
     #Changed columnspan to make text more centered.
-    Label(main_window, text = "Party Hire Tracker", font=(("Arial"), 30, "bold"), relief = SUNKEN, bg = main_color).grid(column = 0, row = 0, columnspan = 5, rowspan = 3)
+    Label(main_window, text = "Party Hire Tracker", font=(("Arial"), 30, "bold"), 
+    relief = SUNKEN, bg = main_color).grid(column = 0, row = 0, columnspan = 5, rowspan = 3)
 
+    #Label for first name and also entry box for first name
     Label(main_window, text="First Name").grid(column=0,row=3)
     entry_firstname = Entry(main_window)
     entry_firstname.grid(column=1,row=3)
 
+    #Label for last name and also entry box for last name
     Label(main_window, text="Last Name") .grid(column=0,row=4, pady =(0, 3))
     entry_lastname = Entry(main_window)
     entry_lastname.grid(column=1,row=4, pady =(0, 3))
 
+    #Label for item hired and also entry box for item hired
     Label(main_window, text="Item Hired") .grid(column=0,row=5)
     entry_hired = Entry(main_window)
     entry_hired.grid(column=1,row=5)
 
+    #Label for item quantity # and also entry box for item quantity #
     Label(main_window, text ="Item Quantity #") .grid(column=0, row=6, pady = (0, 5))
     entry_quantity = Entry(main_window)
     entry_quantity.grid(column =1, row = 6, pady = (0, 5))
     
+    #Label for receipt No. and also creating combobox named delete_item.
+    #delete_item allows for the user to select the receipt number they want to delete
+    #After having selected the receipt number, they click the button which is named "Delete"
+    #as you can tell below.
     Label(main_window, text="Receipt No.") .grid(column=2,row=3)
     Receipt =  StringVar()
     delete_item = ttk.Combobox(main_window, state='readonly', value = receipt_list, textvariable = Receipt)
@@ -66,17 +78,32 @@ def delete_row():
     global delete_item, hire_details, total_entries, Receipt
     global sub_window, name_count, closed_window, receipt_list
     
-
+    #For loop will check the range of the length of the hire_details multidimensional list.
+    #What "i" is saying that if there are any items inside hire_details that is equal to Receipt.get(),
+    #it will the code inside the if statement. Keep in mind that it checks ALL lists inside the multi- 
+    #dimensional lists. 
     for i in range(len(hire_details)):
         if str(hire_details[i][0]) == str(Receipt.get()):
+            #Will get rid of any items inside the hire_details & receipt_list that are equal to the str value of the receipt_number.
             hire_details.pop(i)
             receipt_list.pop(i)
+
+            #As a row is being deleted, total entries will decrease by 1.
             total_entries = total_entries - 1
+
+            #Setting the Receipt StringVar() to empty strings. So default value of delete_item
+            #will be set back to normal (empty) after row has been deleted.
             Receipt.set("")
         
+            #Adding the receipt_number values into the delete_item Combobox.
             delete_item = ttk.Combobox(main_window, state='readonly', value = receipt_list, textvariable = Receipt)
             delete_item.grid(column=3,row=3) 
-
+            
+            #This will destroy & forget ALL widgets inside the sub_window, including everything inside the frame.
+            #After destroying & forgetting all widgets, the program calls the function
+            #print_hire_details(), so all the details from hire_details can be displayed again.
+            #The reason for destroying & forgetting the widgets is to prevent labels from 
+            #overlapping, which results in glitched text.
             for widget in frame.winfo_children():
                 widget.destroy()
             frame.pack_forget()
@@ -95,8 +122,15 @@ def append_details():
     global hire_details, total_entries, delete_item, receipt_number, receipt_list
     global entry_firstname, entry_lastname, entry_quantity, entry_hired
     global Receipt, sub_window
+
+    #Receipt_number below will be a randomly generated 
+    #integer between 10000 & 100000000000.
     receipt_number = random.randint(10000, 100000000000)
     
+    #When details are appended, it will add receipt_number, entry_firstname,
+    #entry_lastname, entry_hired and entry_quantity inside its own
+    #individual lists. Each set of details will have its own list inside the hire_details list,
+    #when appended to hire_details.
     hire_details.append(
         [   
             receipt_number,
@@ -107,16 +141,24 @@ def append_details():
         
         ]
     )
-    
+    #receipt_list will appended receipt_number so that the delete_item Combobox can contain the values
+    #of the receipt numbers, so that the delete button will actually work.
     receipt_list.append(receipt_number)
     delete_item = ttk.Combobox(main_window, state='readonly', value = receipt_list, textvariable = Receipt)
     delete_item.grid(column=3,row=3) 
-    
+    #When the details are appended, the text inside the entry boxes will be removed
+    #This is so the user does not have to erase the text inside when they want
+    #to enter in new details.
     entry_firstname.delete(0, "end")
     entry_lastname.delete(0, "end")
     entry_hired.delete(0, "end")
     entry_quantity.delete(0, "end")
+
+    #Total entries are increased by 1, as details are appended.
     total_entries += 1
+
+    #After details are appended, the below function will run so that the sub_window 
+    #can display the information of the appended customer "hire_details.""
     print_hire_details()
     
 
@@ -138,9 +180,17 @@ def print_hire_details():
         Label(frame, font="bold", text="Last Name").grid(column=2, row=0, padx = (20, 20))
         Label(frame, font="bold", text="Item Hired").grid(column=3, row=0)
         Label(frame, font="bold", text="Item Quantity #").grid(column=4, row=0, padx = (20, 0))
-        sub_window.protocol("WM_DELETE_WINDOW", closed_window) 
-    while name_count < total_entries:
 
+        #Program will know when a window is closed using the x button at the top right side of the window using WM_DELETE_WINDOW,
+        #this is a component to preventing multiple sub_window from opening at a time
+        #After this I have called my closed_window function.
+        sub_window.protocol("WM_DELETE_WINDOW", closed_window) 
+
+    #What this while loop condition is saying that as long as name_count (0) is 
+    #less than total_entries (No. of lists in the multidimensional list hire_details), it will
+    #keep printing the Labels (As GUI on the frame of the sub_window) according to the appropriate
+    #details inside the hire_details list.
+    while name_count < total_entries:
 
         Label(frame, text=(hire_details[name_count][0])).grid(
             column=0, row = name_count + 1
@@ -159,13 +209,24 @@ def print_hire_details():
         )
         
         name_count += 1
-        print(hire_details)
 
+#After the program knows a window has been closed, root_count will be set to 1.
+#Root_count is set to 1 so if user tries to print details with a sub_window already open
+#it won't open due to only a limit of 2 windows being open at a time.
+#The two windows are the main_window and the sub_window
 def closed_window():
     global root_count, sub_window, frame
     root_count = 1
+    
+    #Created a frame inside sub_window. I used a frame so I am able to destroy
+    #widgets. Therefore the details in my sub_window will be under the child frame of the parent sub_window.
     frame = Frame(sub_window)
+
+    #I had to add a sub_window.destroy() as it the sub_window would freeze when I press the x (close) button
+    #on the top right hnad side of the screen.
     sub_window.destroy()
+    
+    
 
 def validity_checker():
     #If all entry boxes are not empty AND entry quantity is a digit, 
@@ -182,7 +243,13 @@ def validity_checker():
             append_details()
         else:
             messagebox.showerror(message ="Sorry! The Item Quantity is restricted between 1 and 500")
-    #If any of the entry boxes are empty and/or entry_quantity is not a digit, it will run this code.
+    #If any of the entry boxes are empty and/or entry_quantity 
+    #is not a digit, it will run this code. If any of the empty boxes are empty,
+    #it will pop up with a error message, telling them what they need to enter into the entry box.
+    #Also checks if quantity is less than 1 or 500 (the restricted amount)
+    #If quantity is less than 1 or 500, error message will pop up telling the user
+    #item quantity is restricted between 1 and 500.
+
     else:
         if len(entry_firstname.get()) == 0:
             messagebox.showerror(message ="Please enter Customer's first name")
@@ -192,8 +259,7 @@ def validity_checker():
             messagebox.showerror(message = "Please enter the item that is being hired")
         if entry_quantity.get().isdigit():
             if (
-                len(entry_quantity.get()) == 0
-                or int(entry_quantity.get()) < 1
+                int(entry_quantity.get()) < 1
                 or int(entry_quantity.get()) > 500
             ):
                 messagebox.showerror(message ="Sorry! The Item Quantity is restricted between 1 and 500")
@@ -201,15 +267,24 @@ def validity_checker():
             messagebox.showerror(message ="Please enter the item quantity (Numbers only)")
 
 def show_details():
+    #Displaying the current details as GUI in the sub_window.
     print_hire_details()
     
     
-
-#Main function that is the first to run to allow for other functions to run its purpose.
+#Main functions contains the nessecary information to allow for the
+#other functions to run without error.
+#Hence when the program runs, this is the first function to run,
+#allowing for the functioning of the other functions.
 def main():
     global main_window, root_count
     global hire_details, receipt_list
     global total_entries, main_color
+
+    #Total entries is used to count how many lists have been added to the multidimensional
+    #list named hire_details. 
+    #Total entries is set to 0 as because no details have been appended to hire_details,
+    
+
     total_entries = 0
     main_color = "cornflower blue"
     
@@ -245,9 +320,14 @@ def main():
     #Second window will be the window uses to display 
     #the party details that were entered in by the user.
     root_count = 1
+
+    #Adding buttons, labels, entries, title, combobox. (GUI of main_window)
     setup_buttons()
+
+    #Displaying main_window to the user
     main_window.mainloop()
 
+#Calling my main function
 main()
 
 
